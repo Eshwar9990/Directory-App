b5622095c2487ab82daf1609ec85b2520918a394
B,EshwarAkhil <eshwarakhil.b@cerner.com>
2018-11-05 10:08:58 -0600

    Modified view hierarchy and added assets, guide modal added with lists of description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,10 @@ declare var $: any;
 
 export class AppComponent {
   currentDirectory = '#';
+  currentDirectoryName: string = '#';
   folderName: string;
+  editFolderName: string;
+  editFolderId: number;
   options = {
     timeOut: 2000,
     showProgressBar: true,
@@ -22,9 +25,16 @@ export class AppComponent {
   
   currentFolders = [];
   showEmptyMessage = true;
+  canBeDeleted: boolean = false;
 
   constructor(private notificationsService: NotificationsService, private foldersService: FoldersService, private cdRef:ChangeDetectorRef, private elRef:ElementRef) {
-    Window["myComponent"] = this;
+    // Window["myComponent"] = this;
+    
+  }
+
+  ngAfterViewInit() {
+    this.setViewHierarchy();
+    $('#guideModal').modal('show');    
   }
 
   createFolder() {
@@ -33,7 +43,7 @@ export class AppComponent {
     } else {
       if(this.foldersService.folders.length == 0) {
         this.foldersService.folders.push({
-          "id" : 1, "parent" : "#", "text" : this.folderName, "isChecked": false
+          "id" : 1, "parent" : "#", "text" : this.folderName, "isChecked": false, "lastModifiedAt": new Date()
         });
       } else {
         for(let i=0;i<this.foldersService.folders.length;i++) {
@@ -43,7 +53,7 @@ export class AppComponent {
           } 
         }
         this.foldersService.folders.push({
-          "id" : this.foldersService.folders[this.foldersService.folders.length-1].id+1, "parent" : this.currentDirectory, "text" : this.folderName, "isChecked": false
+          "id" : this.foldersService.folders[this.foldersService.folders.length-1].id+1, "parent" : this.currentDirectory, "text" : this.folderName, "isChecked": false, "lastModifiedAt": new Date()
         });
       }
       this.getCurrentFolders();
@@ -55,24 +65,56 @@ export class AppComponent {
 
   folderOpen(folderDetails) {
     this.currentDirectory = folderDetails.id;
+    this.currentDirectoryName = folderDetails.text;
     this.getCurrentFolders();    
   }
 
   deleteItems() {
-    this.currentFolders.forEach((item, index) => {
-      if(item.isChecked == true) {
-        this.foldersService.folders.splice(index, 1);
+    let isAllDone = true;
+    for(let i=0;i<this.foldersService.folders.length;i++) {
+      if(this.foldersService.folders.length == 1 && this.foldersService.folders[0].isChecked == true) {
+        this.foldersService.folders = [];
+        this.canBeDeleted = false;
+        this.currentFolders = [];
+        this.getCurrentFolders();
+        return;
+      } else {
+        if(this.foldersService.folders[i].isChecked == true) {
+          this.foldersService.folders.splice(i, 1);
+          isAllDone = false;
+          this.canBeDeleted = true;
+          break;
+        }
       }
-    });
-    this.getCurrentFolders();
+    }
+    if(isAllDone == false) {
+      this.deleteItems();
+    } else {
+      this.getCurrentFolders();
+    }
+  }
+
+  deleteItemsMessage() {
+    this.notificationsService.success('Success!', 'Selected folders are successfully deleted');
   }
 
   folderUp() {
     this.currentFolders = [];
-    let isTopmostFolder = true;
+    let isTopmostFolder = true, temp;
     for(let i=0;i<this.foldersService.folders.length;i++) {
       if(this.foldersService.folders[i].id == this.currentDirectory) {
         this.currentDirectory = this.foldersService.folders[i].parent;
+        temp = this.currentDirectory;
+        for(let j=0;j<this.foldersService.folders.length;j++) {
+          if(this.foldersService.folders[j].id == this.currentDirectory) {
+            temp = this.foldersService.folders[j].parent;
+            this.currentDirectoryName = this.foldersService.folders[j].text;
+            break;
+          } 
+        }
+        if(temp == this.currentDirectory) {
+          this.currentDirectoryName = '#';
+        }
         isTopmostFolder = false;
         break;
       } 
@@ -83,47 +125,42 @@ export class AppComponent {
     this.getCurrentFolders();
   }
 
+  setViewHierarchy() {
+    document.getElementById("quick-access-list").innerHTML = `<img height="150" src="../assets/arrow-pointing-upwards.svg" alt="Upward arrow" style="display: block; margin: auto;">
+    <div style="font-size: 20px; margin: 10vh 0;">
+      You can understand the folder structure hierarchy here after adding atleast one folder
+    </div>
+    <img height="150" src="../assets/downward-arr.svg" alt="Downward arrow" style="display: block; margin: auto;">`;
+  }
+
   getCurrentFolders() { 
+    this.canBeDeleted = false;
     if(this.foldersService.folders.length > 0) {
       this.currentFolders = [];
       let zeroLevelIds = [];
       document.getElementById("quick-access-list").innerHTML = '';
       for(let i=0;i<this.foldersService.folders.length;i++) {
+        this.foldersService.folders[i].isChecked = false;
         if(this.foldersService.folders[i].parent == this.currentDirectory) {
-
           this.currentFolders.push(this.foldersService.folders[i]);
         }
 
-        console.log('ipdu', this.currentFolders);
-
-        
         if(this.foldersService.folders[i].parent == '#') {
-          document.getElementById("quick-access-list").innerHTML += `<a  onclick="Window.myComponent.quickAccessJump(`+this.foldersService.folders[i].parent+`)" href="#" class="list-group-item" id="`+ this.foldersService.folders[i].id +`">`+ this.foldersService.folders[i].text +`</a>`;
+          document.getElementById("quick-access-list").innerHTML += `<a href="#" class="list-group-item" id="`+ this.foldersService.folders[i].id +`">`+ `<img height="15" style="margin: 0 10px 0 0;" src="../assets/is-greater-than-mathematical-sign.svg" alt="Greater than Icon">` + this.foldersService.folders[i].text +`</a>`;
           zeroLevelIds.push(this.foldersService.folders[i].id);
-        } 
+        }         
       }
 
       this.quickAccessUpdation(zeroLevelIds);
-    } 
+    } else {
+      this.setViewHierarchy();
+    }
 
     if(this.currentFolders.length == 0) {
-      this.showEmptyMessage = true;
+      this.showEmptyMessage = true; 
     } else {
       this.showEmptyMessage = false;
     }
-
-    
-  }
-
-  quickAccessJump(selectedParent) {
-    // this.currentFolders = [];
-    // for(let i=0;i<this.foldersService.folders.length;i++) {
-    //   if(this.foldersService.folders[i].id == selectedParent) {
-    //     this.currentDirectory = this.foldersService.folders[i].parent;
-    //   } 
-    // }
-    // this.getCurrentFolders();
-    this.currentFolders = [];
   }
 
   quickAccessUpdation(tempIds) {
@@ -131,19 +168,56 @@ export class AppComponent {
     tempIds.forEach(item => {
       for(let i=0;i<this.foldersService.folders.length;i++) {
         if(this.foldersService.folders[i].parent == item) {     
-          $(`<a href="#" onclick="Window.myComponent.quickAccessJump(`+this.foldersService.folders[i].parent+`)" style="padding-left: `+(parseInt($('#'+item).css("padding-left").replace(/px/,""))+15)+"px"+`;" class="list-group-item" id="`+ this.foldersService.folders[i].id +`">`+ this.foldersService.folders[i].text +`</a>`).insertAfter('#'+item);
+          $(`<a href="#" style="padding-left: `+(parseInt($('#'+item).css("padding-left").replace(/px/,""))+15)+"px"+`;" class="list-group-item" id="`+ this.foldersService.folders[i].id +`">`+ `<img height="15" style="margin: 0 10px 0 0;" src="../assets/is-greater-than-mathematical-sign.svg" alt="Greater than Icon">` + this.foldersService.folders[i].text +`</a>`).insertAfter('#'+item);
           nextLevelIds.push(this.foldersService.folders[i].id);
         }
-
       }
     });
     if(nextLevelIds.length > 0) {
       this.quickAccessUpdation(nextLevelIds);
     }
+  }
+
+  editFolderModal(folder) {
+    this.editFolderName = folder.text;
+    this.editFolderId = folder.id;
+    $('#editModal').modal('show');   
+  }
+
+  editFolder() {
+    for(let i=0;i<this.foldersService.folders.length;i++) {
+      if(this.foldersService.folders[i].id == this.editFolderId) {
+        if(this.foldersService.folders[i].text === this.editFolderName) {
+          this.notificationsService.error('Error!', 'Edited name cannot be the same as old name');
+        } else {
+          this.foldersService.folders[i].text = this.editFolderName;
+          this.foldersService.folders[i].lastModifiedAt = new Date(); 
+          this.notificationsService.success('Success!', 'Folder name edited successfully');
+          this.editFolderId = this.editFolderName = null;
+          this.getCurrentFolders();
+          $('#editModal').modal('hide');   
+        }
+      }
+    }
     
   }
 
-  renameFolder() {
+  closeDeleteModal() {
+    $('#deleteModal').modal('hide');   
+  }
 
+  confirmDeletion() {
+    let notChecked = true;
+    for(let i=0;i<this.currentFolders.length;i++) {
+      if(this.currentFolders[i].isChecked == true) {
+        $('#deleteModal').modal('show');   
+        notChecked = false;
+        return;
+      } 
+    }
+
+    if(notChecked == true) {
+      this.notificationsService.error('Error!', 'Please select at least one folder to delete');
+    }
   }
 }
